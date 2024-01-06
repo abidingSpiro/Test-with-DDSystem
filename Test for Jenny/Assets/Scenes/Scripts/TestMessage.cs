247545d558ccf4cbe4a1d1c21a4f0ad1e4071bb8
abidingSpiro <abidingspiro@gmail.com>
2024-01-06 01:35:47 -0500

    added files

---
+++ Test for Jenny/Assets/Scenes/Scripts/TestMessage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Doublsb.Dialog;
+
+public class TestMessage : MonoBehaviour
+{
+    public DialogManager DialogManager;
+
+    public GameObject[] Example;
+
+    private void Awake()
+    {
+        var dialogTexts = new List<DialogData>();
+
+                
+        dialogTexts.Add(new DialogData("welp, i found a cool dialogue system.", "Li"));
+
+        dialogTexts.Add(new DialogData("/emote:Happy/ Something like this? ", "Li"));
+
+        dialogTexts.Add(new DialogData("I think I can get the grid working tomorrow", "Li"));
+
+        dialogTexts.Add(new DialogData("/emote:Happy/ that's all. buenas noches", "Li"));
+
+        DialogManager.Show(dialogTexts);
+    }
+
+    private void Show_Example(int index)
+    {
+        Example[index].SetActive(true);
+    }
+}
